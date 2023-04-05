f9277fd986ef6f9eb759ed26f973105cf6f140e7
Chandler Mayo <chandler@pubnub.com>
2023-04-05 10:16:33 -0400

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -40,7 +40,6 @@
       }]
     },
     options: {
-      maintainAspectRatio: false,
       legend: {
         display: true
       }
@@ -67,7 +66,6 @@
       }]
     },
     options: {
-      maintainAspectRatio: false,
       legend: {
         display: true
       }
@@ -293,7 +291,7 @@ document.getElementById('staticBackdrop').addEventListener('hidden.bs.modal', ()
   actionCompleted({action: 'Read Demo Info', windowLocation: window.location.href}); // This is for the interactive demo on PubNub.com. It is not part of the demo of this application. 
 })
 document.getElementById('VStatModal').addEventListener('shown.bs.modal', () => {
-  actionCompleted({action: 'View Operational Info', windowLocation: window.location.href}); // This is for the interactive demo on PubNub.com. It is not part of the demo of this application. 
+  actionCompleted({action: 'View Vibration Info', windowLocation: window.location.href}); // This is for the interactive demo on PubNub.com. It is not part of the demo of this application. 
 })
 document.getElementById('OPStatModal').addEventListener('shown.bs.modal', () => {
   actionCompleted({action: 'View Operational Info', windowLocation: window.location.href}); // This is for the interactive demo on PubNub.com. It is not part of the demo of this application. 
